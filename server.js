8916e95806f449aa6e4885b692f281cadec99fdd
jadc019 <jadc019@gmail.com>
2017-02-23 13:43:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http')
- var port = process.env.PORT || 1337;
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello World\n');
- }).listen(port);
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 80
+const app = express()
+
+// serve static assets normally
+app.use(express.static(__dirname + '/public'))
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port)
+console.log("server started on port " + port)
